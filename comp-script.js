e7500451fd9ba39a0892d224d355c24033905e34
BreckB1013 <bcbeck42@gmail.com>
2024-11-20 08:11:58 -0800

    Add files via upload

---
+++ comp-script.js
@@ -0,0 +1,29 @@
+let firstName = prompt("Please enter your first name:");
+        let number = parseInt(prompt("Please enter a number between 1 and 100:"));
+
+        if (number % 2 === 0) {
+            console.log(`${number} is even`);
+        } else {
+            console.log(`${number} is odd`);
+        }
+
+        if (number > 50) {
+            console.log(`${number} is greater than 50`);
+        } else {
+            console.log(`${number} is less than or equal to 50`);
+        }
+
+        for (let i = 1; i < number; i++) {
+            console.log(i);
+        }
+
+        for (let i = 100; i >= number; i--) {
+            console.log(i);
+        }
+
+        if (firstName === "Breck") {
+            alert("That's a great name!");
+        }
+
+        let reversedName = firstName.split('').reverse().join('');
+        console.log(`Your name in reverse is: ${reversedName}`);
